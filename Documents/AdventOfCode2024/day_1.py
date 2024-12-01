56775a316cdb8c66d5b620f12c71b127b0c1a97a
Kabotan <justinleddet@gmail.com>
2024-12-01 09:08:37 +0100

    Add day 1

--- Documents/AdventOfCode2024/day_1.py
+++ Documents/AdventOfCode2024/day_1.py
@@ -1,9 +1,23 @@
 f = open('day_1_input.txt','r')
 lines = f.readlines()
 
+lines = [ x.split() for x in lines]
 
 #-----------part 1--------------------------------------
 
+left = sorted([int(pair[0]) for pair in lines])
+right = sorted([int(pair[1]) for pair in lines])
+
+diff = [ abs(x - y) for x,y in zip(left, right)]
+
+print(sum(diff))
 
 
 #-----------part 2--------------------------------------
+
+similarity_score = 0 
+
+for element in left:
+    similarity_score += element * right.count(element)
+    
+print(similarity_score)
